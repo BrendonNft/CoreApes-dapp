5636e1d1ab64f9bd74e74d9094c01e7cfd572a56
ApeStak <127142891+ApeStak@users.noreply.github.com>
2023-03-07 07:47:24 +0100

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -25,9 +25,9 @@ export const fetchData = () => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let totalStaked = await store
+      let Supply = await store
         .getState()
-        .blockchain.smartContract.methods.totalStaked()
+        .blockchain.smartContract.methods.Supply()
         .call();
       // let cost = await store
       //   .getState()
@@ -36,7 +36,7 @@ export const fetchData = () => {
 
       dispatch(
         fetchDataSuccess({
-          totalStaked,
+          Supply,
           // cost,
         })
       );

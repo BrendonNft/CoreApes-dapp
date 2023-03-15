aca51885ea6e10e2c6c25f646d46fc6bc5f4f408
ApeStak <127142891+ApeStak@users.noreply.github.com>
2023-03-15 03:47:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`claim processing...`);
     blockchain.smartContract.methods
-      .claimRewards()
+      .claimReward()
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
